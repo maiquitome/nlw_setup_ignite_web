d24ecfe97167a450c6250f60993018cf7d9ebee5
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2023-01-19 15:36:55 -0300

    feat: :sparkles: add HabitDay & SummaryTable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-// import { Habit } from './components/Habits'
-
 import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
 export function App() {
   return (
-    <Header />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,10 @@ module.exports = {
     extend: {
       colors: {
         my_app: '#09090A',
+      },
+      
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))',
       }
     },
   },
