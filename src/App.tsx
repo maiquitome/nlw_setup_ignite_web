9d55ed0d282056a1ef4322696f3de452b6a7af66
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2023-01-19 13:58:22 -0300

    feat: :sparkles: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './styles/global.css'
-import { Habit } from './components/Habits'
+// import { Habit } from './components/Habits'
+
+import { Header } from "./components/Header";
 
 export function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={10} />
-      <Habit completed={20} />
-      <Habit completed={30} />
-    </div>
+    <Header />
   )
 }
 
